6b490cd3821f7bbf4459dcaa0629ec5ddb992524
Camboe <s205360@student.dtu.dk>
2021-11-17 19:12:03 +0100

    added sta.frag and navigation

--- app/src/main/java/com/example/s205360lykkehjulet/MainActivity.kt
+++ app/src/main/java/com/example/s205360lykkehjulet/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.s205360lykkehjulet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.s205360lykkehjulet.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
