26fa2e75595f84f5f0c683419ff5374612d4d35c
Camboe <s205360@student.dtu.dk>
2021-11-22 11:02:55 +0100

    new class GameViewHolder for logic

---
+++ app/src/main/java/com/example/s205360lykkehjulet/game/GameViewHolder.kt
@@ -0,0 +1,14 @@
+package com.example.s205360lykkehjulet.game
+
+class GameViewHolder {
+    var word: String = "null"
+    constructor(){
+        //TODO Make random
+        word = "test"
+    }
+
+    fun generateWord(){
+        word = "test"
+    }
+
+}
