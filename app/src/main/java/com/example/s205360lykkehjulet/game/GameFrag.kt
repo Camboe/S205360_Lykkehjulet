dd8648ce86cf6786289e19a75e65143f84db3adc
Camboe <s205360@student.dtu.dk>
2021-11-27 20:25:35 +0100

    added binding to access the view in the fragment, get function to the buttons in the gameFragment

--- app/src/main/java/com/example/s205360lykkehjulet/game/GameFrag.kt
+++ app/src/main/java/com/example/s205360lykkehjulet/game/GameFrag.kt
@@ -5,56 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.example.s205360lykkehjulet.R
+import com.example.s205360lykkehjulet.databinding.ActivityMainBinding
+import com.example.s205360lykkehjulet.databinding.FragmentGameBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GameFrag.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GameFrag : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    // private val points_at_start
+    // lav en liste af bogstaver.
+    // lav en liste af ord.
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+
+    //reference til GameViewHolder
+    private val viewModel : GameViewHolder by viewModels ()
+
+    // gives access to view in fragment_game
+    private lateinit var binding: FragmentGameBinding
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+
+
+        // random choose word:
+        val category = context?.resources?.getString(requireArguments().getInt("Title"))
+
+        viewModel.generateWord(category!!)
+
+
+        binding.textView.text = viewModel.newWord().toString()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameFrag.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameFrag().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.kobVokal.setOnClickListener {
+
+        }
+        binding.spinArrow.setOnClickListener{ spinWheel() }
+        binding.getOrd.setOnClickListener{}
+
     }
+
+    fun spinWheel(){
+        Toast.makeText(activity, "spin igen", Toast.LENGTH_SHORT).show()
+
+    }
+
+    fun guessWord () {
+
+    }
+
+    fun buyVocal () {
+
+    }
+
+
 }
