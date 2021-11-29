353bbc2d365ee72ef5c5b29404a76d8fb44e35d3
Camboe <s205360@student.dtu.dk>
2021-11-29 14:07:48 +0100

    modified the fieldList

---
+++ app/src/main/java/com/example/s205360lykkehjulet/Data/FieldsOnWheel.kt
@@ -0,0 +1,4 @@
+package com.example.s205360lykkehjulet.Data
+
+val fieldsList: List<String> =
+    listOf("500 POINT", "FALLIT", "1500 POINT", "2000 POINT", "EKSTRA LIV", "750 POINT", "1000 POINT", "MISTET LIV")
