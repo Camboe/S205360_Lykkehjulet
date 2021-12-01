d0f63518213d14f1a332db0da1e8c834a7d75dc7
Camboe <s205360@student.dtu.dk>
2021-12-01 01:12:10 +0100

    updated the lists

--- app/src/main/java/com/example/s205360lykkehjulet/Data/ListofWords.kt
+++ app/src/main/java/com/example/s205360lykkehjulet/Data/ListofWords.kt
@@ -1,4 +1,13 @@
 package com.example.s205360lykkehjulet.Data
 
-val wordList: List<String> =
-    listOf("Madonna", "Britney Spears", "Medina", "Nik og Jay")
+val famousList: List<String> =
+    listOf("madonna", "britney spears", "beyonce", "brad pitt")
+
+val bookTitlesList: List<String> =
+    listOf("harry potter", "twilight", "da vinci code", "chocolat")
+
+val sportList: List<String> =
+    listOf("fodbold", "tennis", "volleyball", "rugby")
+
+val seveardighederList: List<String> =
+    listOf("frihedsgudinden", "notre Dame", "colosseum", "eiffeltårnet")
